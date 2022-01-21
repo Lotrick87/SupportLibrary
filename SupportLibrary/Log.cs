6aea0c1294834f5fae63d7ae394d24b62ab77502
Lotrick87 <77890914+Lotrick87@users.noreply.github.com>
2022-01-21 06:36:11 +0100

    Add project files.

---
+++ SupportLibrary/Log.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SupportLibrary
+{
+    public class Log
+    {
+
+        private static Action<TraceEventType, object, object[]> _log;
+
+        public Log(Func<string, object, object> api)
+        {
+            _log = api("logger", "TraceEventType") as Action<TraceEventType, object, object[]>;
+        }
+
+        public static void Info(string textToWrite)
+        {
+            _log(TraceEventType.Information, textToWrite, null);
+        }
+
+        public static void Warning(string textToWrite)
+        {
+            _log(TraceEventType.Warning, textToWrite, null);
+        }
+
+        public static void Error(Exception exception, string textToWrite = null)
+        {
+            if (textToWrite != null)
+            {
+                _log(TraceEventType.Error, textToWrite, null);
+            }
+            _log(TraceEventType.Error, exception, null);
+        }
+    }
+}
---
+++ SupportLibrary/Support.cs
@@ -0,0 +1,77 @@
+﻿using Base.Core;
+using Base.Defs;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using static System.Reflection.BindingFlags;
+
+namespace SupportLibrary
+{
+    public class Support
+    {
+        public static List<T> GetDefsList<T>() where T : BaseDef
+        {
+            return GameUtl.GameComponent<DefRepository>().GetAllDefs<T>().ToList();
+        }
+
+        public static D GetDef<D>(string defName) where D : BaseDef
+        {
+            return GameUtl.GameComponent<DefRepository>().GetAllDefs<D>().Where(x => string.Equals(x.name, defName, StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+        }
+
+        public static void CreateDef<T>(T defToCreate) where T : BaseDef
+        {
+            GameUtl.GameComponent<DefRepository>().CreateRuntimeDef(defToCreate, typeof(T), defToCreate.Guid);
+
+            T defToRename = GetDefWithContains<T>(defToCreate.name);
+
+            int position = defToRename.name.IndexOf("(Clone)", StringComparison.OrdinalIgnoreCase);
+            Log.Info($"position of clone: {position}");
+            if (position >= 0)
+            {
+                defToRename.name = defToRename.name.Remove(position);
+                Log.Info($"renaming to: {defToRename.name}");
+            }
+        }
+
+        private static D GetDefWithContains<D>(string defName) where D : BaseDef
+        {
+            return GameUtl.GameComponent<DefRepository>().GetAllDefs<D>().Where(x => x.name.IndexOf(defName, StringComparison.OrdinalIgnoreCase) >= 0).FirstOrDefault();
+        }
+
+        public static string GetDefName(string input)
+        {
+            if (input.Contains(":"))
+            {
+                string[] splitter = input.Split(':');
+                for (int i = 0; i < splitter.Length; i++)
+                {
+                    if (splitter[i].IndexOf("Def", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        return splitter[i].Trim();
+                    }
+                }
+
+                return splitter[1].Trim();
+            }
+            else
+            {
+                return input.Trim();
+            }
+        }
+
+
+        public static HarmonyMethod HarmonyMethod<H>(string name) => new HarmonyMethod(Method<H>(name));
+
+        public static MethodInfo Method<T>(string name, Type[] parameters = null)
+        {
+            if (parameters == null)
+            {
+                return typeof(T).GetMethod(name, Public | NonPublic | Instance | Static);
+            }
+            return typeof(T).GetMethod(name, parameters);
+        }
+    }
+}
